a8b2e5e5c096f9e21351021c59d104483f495090
eli 3 <157757893+Canuto371@users.noreply.github.com>
2025-02-25 17:51:34 -0600

    Update decoder_7_seg.v

--- Decodificador_BCD/decoder_7_seg.v
+++ Decodificador_BCD/decoder_7_seg.v
@@ -1,20 +1,20 @@
 module decoder_7_seg(
-	input [2:0] decoder_in,
+	input [9:0] decoder_in, 
 	output reg [0:6] decoder_out
 );
 	always @(*)
 	begin
 		case(decoder_in)
-			0:decoder_out=1;
-			1:decoder_out=7'b1001111;
-			2:decoder_out=7'b0010010;
-			3:decoder_out=7'b0000110;
-			4:decoder_out=7'b1001100;
-			5:decoder_out=7'b0100100;
-			6:decoder_out=7'b0100000;
-			7:decoder_out=7'b0001111;
-			8:decoder_out=7'b0000000;
-			9:decoder_out=7'b0000100;
+			 0: decoder_out = 7'b0000001; 
+		         1: decoder_out = 7'b1001111; 
+		         2: decoder_out = 7'b0010010; 
+		         3: decoder_out = 7'b0000110; 
+		         4: decoder_out = 7'b1001100; 
+		         5: decoder_out = 7'b0100100; 
+		         6: decoder_out = 7'b0100000; 
+		         7: decoder_out = 7'b0001111; 
+		         8: decoder_out = 7'b0000000; 
+		         9: decoder_out = 7'b0000100; 
 			//10:decoder_out=7'b0001000;
 			//11:decoder_out=7'b1100000;
 			//12:decoder_out=7'b0110001;
